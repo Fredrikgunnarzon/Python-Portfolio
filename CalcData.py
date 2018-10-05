308c3d41ee9580a49a39035678cf75f25f7fb74b
Fredrik Gunnarsson <fredrikgunnarsson@outlook.com>
2018-10-06 01:15:42 +0200

    Plotting

--- CalcData.py
+++ CalcData.py
@@ -45,3 +45,49 @@ def Moving_Average(stocks,Stock_Data,Average):
 	else:
 		print("\n NOTHING TO DO, ALL ABOVE MA"+str(Average)+"\n")
 
+def rsi(prices, n = 14):
+
+    deltas = np.diff(prices)
+    seed = deltas[:n+1]
+    up = seed[seed >= 0].sum()/n
+    down = -seed[seed < 0].sum()/n
+    rs = up/down
+    rsi = np.zeros_like(prices)
+    rsi[:n] = 100. - 100./(1. + rs)
+
+    for i in range(n, len(prices)):
+        delta = deltas[i - 1]  # cause the diff is 1 shorter
+
+        if delta > 0:
+            upval = delta
+            downval = 0.
+        else:
+            upval = 0.
+            downval = -delta
+
+        up = (up*(n - 1) + upval)/n
+        down = (down*(n - 1) + downval)/n
+
+        rs = up/down
+        rsi[i] = 100. - 100./(1. + rs)
+
+    return rsi
+
+def movingaverage(values,window):
+    weigths = np.repeat(1.0, window)/window
+    smas = np.convolve(values, weigths, 'valid')
+    return smas
+
+
+def ExpMovingAverage(values, window):
+    weights = np.exp(np.linspace(-1., 0., window))
+    weights /= weights.sum()
+    a =  np.convolve(values, weights, mode='full')[:len(values)]
+    a[:window] = a[window]
+    return a
+
+
+def computeMACD(x, slow = 26, fast = 12):
+    emaslow = ExpMovingAverage(x, slow)
+    emafast = ExpMovingAverage(x, fast)
+    return emaslow, emafast, emafast - emaslow
--- portfolio_plot.py
+++ portfolio_plot.py
@@ -6,6 +6,8 @@ import numpy as np
 import matplotlib.cbook as cbook
 import csv
 import matplotlib.dates as mdates
+import CalcData
+from mpl_finance import candlestick_ohlc
 pd.set_option('display.height', 1000)
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
@@ -56,23 +58,44 @@ def plot_stock(Data,stock):
 	Legends = pd.DataFrame({'Tickers':[]})
 	Average1= 10
 	Average2= 20
-
 	Moving_10 = np.round(pd.rolling_mean(Data['Adj Close: '+ stock.Tickers],window=Average1),10)
 	Moving_20 = np.round(pd.rolling_mean(Data['Adj Close: '+ stock.Tickers],window=Average2),20)
-	#Moving_10.plot(grid=True)
-	#Moving_20.plot(grid=True)
-	plt.plot(Data.Date,Moving_10)
-	plt.plot(Data.Date,Moving_20)
-	Data['Adj Close: '+stock.Tickers].plot(grid=True)
+	
 
-	Legends.loc[len(Legends)] = stock.Tickers+": MA"+str(Average1)
-	Legends.loc[len(Legends)] = stock.Tickers+": MA"+str(Average2)
-	Legends.loc[len(Legends)] = stock.Tickers
 
-	plt.legend(Legends.Tickers)
-	#plt.gcf().autofmt_xdate()
-	plt.xaxis.set_minor_locator(months)
+	left, width = 0.1, 0.8
+	axescolor = '#f6f6f6'
+	rect1 = [left, 0.7, width, 0.2]
+	rect2 = [left, 0.3, width, 0.4]
+	fig=plt.figure(facecolor='#07000d')
+	ax1 = fig.add_axes(rect1)  # left, bottom, width, height
+	ax2 = fig.add_axes(rect2, sharex=ax1)
+	fillcolor = 'darkgoldenrod'
 
+	rsi = CalcData.rsi(Data["Adj Close: "+stock.Tickers])
+	ax1.plot(Data.Date, rsi, color=fillcolor)
+	ax1.axhline(70, color=fillcolor)
+	ax1.axhline(30, color=fillcolor)
+	ax1.fill_between(Data.Date, rsi, 70, where=(rsi >= 70), facecolor=fillcolor, edgecolor=fillcolor)
+	ax1.fill_between(Data.Date, rsi, 30, where=(rsi <= 30), facecolor=fillcolor, edgecolor=fillcolor)
+	ax1.text(0.6, 0.9, '>70 = overbought', va='top', transform=ax1.transAxes, fontsize=9)
+	ax1.text(0.6, 0.1, '<30 = oversold', transform=ax1.transAxes, fontsize=9)
+	ax1.set_ylim(0, 100)
+	ax1.set_yticks([30, 70])
+	ax1.text(0.025, 0.95, 'RSI (14)', va='top', transform=ax1.transAxes, fontsize=9)
+	ax1.set_title('%s daily' % stock.Tickers)
+
+
+	ax2.plot(Data.Date,Moving_10)
+	ax2.plot(Moving_20)
+	# Data['Adj Close: '+stock.Tickers].plot(grid=True)
+
+	# Legends.loc[len(Legends)] = stock.Tickers+": MA"+str(Average1)
+	# Legends.loc[len(Legends)] = stock.Tickers+": MA"+str(Average2)
+	# Legends.loc[len(Legends)] = stock.Tickers
+
+	# plt.legend(Legends.Tickers)
+	# plt.gcf().autofmt_xdate()
 	plt.show()
 
 
